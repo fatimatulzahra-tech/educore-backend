9fe7fcead0b8f9ae39fb8750f1a71d13c34efe36
Fatima Tul Zahra <fatimatulzahra.tech@gmail.com>
2026-07-23 15:41:58 -0700

    backend middleware update

--- app/main.py
+++ app/main.py
@@ -59,9 +59,15 @@ app.state.limiter = limiter
 # -------------------------
 # MIDDLEWARE (CLEANED)
 # -------------------------
+# -------------------------
+# MIDDLEWARE
+# -------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://educoresap.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
