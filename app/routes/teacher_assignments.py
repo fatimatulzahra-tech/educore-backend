43961de41730e0a96b3c30dc7c3e9baeb5f3a980
Fatima Tul Zahra <fatimatulzahra.tech@gmail.com>
2026-07-24 10:49:49 -0700

    route fix

--- app/routes/teacher_assignments.py
+++ app/routes/teacher_assignments.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
+from typing import Optional
 from app.database.database import get_db
 from app.models.class_model import Class
 from app.models.section_model import Section
